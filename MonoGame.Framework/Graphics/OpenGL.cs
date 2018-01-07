1db4f705ca9eedff6d825d98f8dcc1745d504c90
Jan Kotas <jkotas@microsoft.com>
2018-01-07 07:36:19 -0800

    Use generic Marshal.GetDelegateForFunctionPointer when compiling for netstandard
    The generic Marshal.GetDelegateForFunctionPointer is AOT compilation friendly.

--- MonoGame.Framework/Graphics/OpenGL.cs
+++ MonoGame.Framework/Graphics/OpenGL.cs
@@ -1446,7 +1446,11 @@ namespace MonoGame.OpenGL
                 var addr = EntryPointHelper.GetAddress(proc);
                 if (addr == IntPtr.Zero)
                     return null;
+#if NETSTANDARD
+                return Marshal.GetDelegateForFunctionPointer<T>(addr);
+#else
                 return Marshal.GetDelegateForFunctionPointer(addr, typeof(T));
+#endif
             }
             catch (EntryPointNotFoundException)
             {
