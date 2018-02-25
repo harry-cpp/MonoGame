0b348f62885754ac4b13f6de75520b3f60225e87
cra0zy <cra0zy@gmail.com>
2018-02-25 17:41:32 +0100

    Fix templates and version bump

--- MonoGame.Framework/Graphics/OpenGL.cs
+++ MonoGame.Framework/Graphics/OpenGL.cs
@@ -1448,7 +1448,7 @@ namespace MonoGame.OpenGL
                     return null;
                     
 #if NETSTANDARD
-                return Marshal.GetDelegateForFunctionPointer<T>(addr) as System.Delegate;
+                return Marshal.GetDelegateForFunctionPointer<T>(addr);
 #else
                 return (T)(object)Marshal.GetDelegateForFunctionPointer(addr, typeof(T));
 #endif
