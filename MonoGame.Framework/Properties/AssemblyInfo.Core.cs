af38d2f56755ac978c4d6ba62d56270b779aff94
Bradley Smith <brad@brad-smith.co.uk>
2018-05-27 00:13:41 +0100

    Add MonoGame.Framework.Content.Pipeline.Core package

---
+++ MonoGame.Framework/Properties/AssemblyInfo.Core.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+// Allow the content pipeline assembly to access
+// some of our internal helper methods that it needs.
+[assembly: InternalsVisibleTo("MonoGame.Framework.Content.Pipeline")]
